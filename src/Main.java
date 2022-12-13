3348bf31395cd1d647d00fce84959a2789a6563c
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-13 09:53:33 +0100

    Commit init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String string1 = "tup tuup tuuup tuuuup";
+        String string2 = string1.replaceAll("u{2,3}","x");
+        System.out.println(string2);
+    }
+}
